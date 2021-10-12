ebbcb2f4784662d943c1a1dd201f7c73fa41166e
mihkel.martin <mihkelmartin@gmail.com>
2021-10-12 10:11:44 +0300

    teine commit, midagi on

--- vormel.cpp
+++ vormel.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int M, N, K;
 int main() {
     cin>>M>>N>>K;
+    // Loo massiv ringiaegadele
     int **a = new int*[M];
     for (int i = 0; i < M; ++i) {
         a[i] = new int[N];
     }
+
+    // Loe sisse ja täida
     for (int i = 0; i < M; ++i) {
         int r, d;
         cin>>r>>d;
         a[i][0]=r;
         for (int j = 1; j < N; ++j) {
-            a[i][j]= a[i][j-1] + r + j * d;
+            int u = a[i][j-1] + r + j * d;
+
+            int k = j;
+            int kprm = 0;
+            int v = INT32_MAX;
+            while (k > 0) {
+                int vtmp = a[i][k-1] + a[i][j-k] + K;
+                if(vtmp < v){
+                    v = a[i][k-1] + a[i][j-k] + K;
+                    kprm = k + 1;
+                }
+                k--;
+            }
+            if(v<u){
+                cout<<"Leidsime "<< i << " "<<j << " " << kprm;
+            }
+            a[i][j]= std::min(u, v);
         }
     }
 
