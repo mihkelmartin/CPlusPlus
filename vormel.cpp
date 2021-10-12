b2bc47594e224a4a08e1ba2be6115cc0b780123f
mihkel.martin <mihkelmartin@gmail.com>
2021-10-12 14:24:58 +0300

    Vilets seis, väga vilets

--- vormel.cpp
+++ vormel.cpp
@@ -7,12 +7,8 @@ int main() {
     cin>>M>>N>>K;
     // Loo massiv ringiaegadele
     int **a = new int*[M];
-    int **s = new int*[M];
-    int **b = new int*[M];
     for (int i = 0; i < M; ++i) {
         a[i] = new int[N];
-        s[i] = new int[N];
-        b[i] = new int[2];
     }
 
 
@@ -21,30 +17,29 @@ int main() {
         int r, d;
         cin>>r>>d;
         a[i][0]=r;
-        s[i][0]=0;
-        int rpv = 1;
+        int kparim = 0;
+        int lisakordaja = 1;
+        int viimane_vahetus = INT32_MIN;
         for (int j = 1; j < N; ++j) {
-            s[i][j] = s[i][j-1];
-            int u = a[i][j-1] + r + rpv * d;
-            rpv++;
+            int u = a[i][j-1] + r + lisakordaja * d;
 
+            lisakordaja++;
             int k = j;
-            int kprm = 0;
             int v = INT32_MAX;
-            while (k > 0) {
+            // k < ?
+            while (k > 0 && k > viimane_vahetus) {
                 int vtmp = a[i][k-1] + a[i][j-k] + K;
                 if(vtmp < v){
                     v = a[i][k-1] + a[i][j-k] + K;
-                    kprm = j - k + 1;
+                    kparim = k;
                 }
                 k--;
             }
             if(v<u){
-                b[i][0] = kprm;
-                s[i][j] = j;
-                // Arv paremaid peale vahetust
-                b[i][1] = j - kprm + 1;
-                rpv = j - kprm + 1;
+                // k kohal vahetus k=0,k+1=1
+                cout<<"Vahetus: " << j;
+                lisakordaja = j - kparim + 1;
+                viimane_vahetus = kparim + 1;
                 u = v;
             }
             a[i][j]= u;
@@ -58,8 +53,6 @@ int main() {
     for (int i = 0; i < M; ++i) {
         for (int j = 0; j < N; ++j) {
             cout<<a[i][j]<<" "<<endl;
-            cout<<s[i][j]<<" ";
-
         }
         cout<<endl;
     }
